802ba65f52c44533308882940ac45e25c9e92449
Andres Cervera <andres.cervera.rozo@gmail.com>
2023-02-10 15:07:38 -0500

    Syntax fixes

--- mie443_contest1/src/contest1.cpp
+++ mie443_contest1/src/contest1.cpp
@@ -43,36 +43,36 @@
 #define B_THRESHOLD 0.65                        // Considered wall in the window for passage
 #define C_THRESHOLD 0.7788                      // Side (30 degree) distances to check for free space
 #define ODOM_ARRAY_LENGTH 50                    // Max number of checkpoints per trial
-#define FORWARD_FAST_V 0.6   //!!!!!                  // Velocity when far from walls
-#define FORWARD_SLOW_V 0.4   //!!!!!                  // Velocity near walls
+#define FORWARD_FAST_V 0.6   //!!!!!            // Velocity when far from walls
+#define FORWARD_SLOW_V 0.4   //!!!!!            // Velocity near walls
 #define STEP_SIZE 0.2                           // Size of Forward steps (m)
 #define BACKWARD_V -0.05                        // Magnitude and direction in x axis
 #define BACKWARD_T KOBUKI_DIAM /2 /BACKWARD_V   // Move backwards a distance == to the radius of the turtlebot
-#define LOOP_RATE 3
-#define TURNING_V 1         //!!!!!                  // Rad/s
+#define LOOP_RATE 10                            // Rate for while loops that dictate callback and publish frequency (Hz)
+#define TURNING_V 1         //!!!!!             // Rad/s
 #define MAX_OBST_DIST 0.5                       // If less than this, we should turn, meters
-#define STOPS_SPACING 2     //!!!!!                  // Distance between checkpoints, meters
+#define STOPS_SPACING 2     //!!!!!             // Distance between checkpoints, meters
 #define TURNING_LIM 3000                        // Amounts of total degrees turned before turning robot around
 #define OCCUP_WEIGHT 10                         // Weights for each factor
 #define ODOM_WEIGHT 10
 #define SCAN_WEIGHT 10
 #define ADJUST_ANGLE 15                         // How much to adjust robot by when wall detected on sides, degrees
-// #define CHECKPOINT_RADIUS 0.2                  // How close are we to a previous checkpoint, may remove
+// #define CHECKPOINT_RADIUS 0.2                // How close are we to a previous checkpoint, may remove
 const int CENTRE_INDEX = SCAN_LENGTH / 2 -1;    // Get index for the middle index for the A value
 
 
 //!!!!!!!!!!!GLOBAL VARIABLES!!!!!!!!!!!!!!!!!!!!!!!!!
-uint8_t l_state[3] = {1,0,0};
-uint8_t lm_state[3] = {1,1,0};
-uint8_t r_state[3] = {0,0,1};
-uint8_t rm_state[3] = {0,1,1};
-uint8_t m_state[3] = {0,1,0};
-uint8_t lmr_state[3] = {1,1,1};
-uint8_t lr_state[3] = {1,0,1};
+uint8_t LState[3] = {1,0,0};
+uint8_t LMState[3] = {1,1,0};
+uint8_t RState[3] = {0,0,1};
+uint8_t RMState[3] = {0,1,1};
+uint8_t MState[3] = {0,1,0};
+uint8_t LMRState[3] = {1,1,1};
+uint8_t LRState[3] = {1,0,1};
 
 uint8_t PressedBumper[3]={0,0,0};               // 3 bit array of bumper status
-std::vector<float> CurrOdom{0, 0};           // Where the turtlebot is in that moment
-// std::vector<float> XboxRanges = {0};             // Storage for scan topic messages
+std::vector<float> CurrOdom{0, 0};              // Where the turtlebot is in that moment
+// std::vector<float> XboxRanges = {0};         // Storage for scan topic messages
 std::vector<std::vector<float>> OdomArray{ODOM_ARRAY_LENGTH, std::vector<float>(2)}; // Full Array saving all checkpoint locations
 
 bool IsNearWall = true;                         // Can we go fast or not
@@ -93,7 +93,7 @@ float BPlus = 0;
 float CPlus = 0;
 
 ros::Publisher VelPub;
-geometry_msgs::Twist Vel;                                       //create message for velocities as Twist type
+geometry_msgs::Twist Vel;                                       // Create message for velocities as Twist type
 
 // !!!!!!!!!!!!DECLARE FUNCTIONS!!!!!!!!!!!!!!!!!!!!!!!
 void reverse(ros::Publisher VelPub);
@@ -103,7 +103,7 @@ void rotate(ros::Publisher VelPub, int angle);
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
     // Access using bumper[kobuki_msgs::BumperEvent::{}] LEFT, CENTER, or RIGHT
-    Bumper[msg->bumper] = msg->state;                           //assigns bumper array to match message based on msg message
+    Bumper[msg->bumper] = msg->state;                           // Assigns bumper array to match message based on msg message
     AnyBumperPressed = false;
     for (uint8_t i = 0; i < N_BUMPER; i++){
         if (Bumper[i] == kobuki_msgs::BumperEvent::PRESSED){
@@ -141,7 +141,7 @@ void odomCallback (const nav_msgs::Odometry::ConstPtr& msg)
 
 
 //!!!!!!!!!USER-DEFINED FUNCTIONS!!!!!!!!!!!!!!!!!!!!!!!!
-bool array_equal(uint8_t array_1[], uint8_t array_2[]) {
+bool arrayEqual(uint8_t array_1[], uint8_t array_2[]) {
     for (int i =0; i< 2; i++){
         if (array_1[i] != array_2[i] ) {
             return false;
@@ -168,27 +168,25 @@ float distance(std::vector<float> prev_odom, std::vector<float> new_odom){     /
     float B = (new_odom[1] - prev_odom[1]);
     float C_squared = A*A + B*B;
     // ROS_INFO("A: %f, B: %f, C_squared: %f", A, B, C_squared);
-    if (C_squared) return (std::sqrt(C_squared));
-    else return 0;
-    
+    return (std::sqrt(C_squared));    
 }
 
 
 int forward(ros::Publisher VelPub, float linear_vel, float distance)
 {
     ros::Rate loop_rate(LOOP_RATE);
-    std::chrono::time_point<std::chrono::system_clock> start;       //initialize timer
+    std::chrono::time_point<std::chrono::system_clock> start;       // Initialize timer
 
     Vel.angular.z = 0.0;
     Vel.linear.x = linear_vel;
-    uint64_t secondsElapsed = 0;                                    //variable for time that has passed
+    uint64_t secondsElapsed = 0;                                    // Variable for time that has passed
     float time_forward = abs(distance/linear_vel);
 
-    start = std::chrono::system_clock::now();                       //start the timer
+    start = std::chrono::system_clock::now();                       // Start the timer
 
     while (ros::ok() && secondsElapsed <= time_forward && !AnyBumperPressed) {
         VelPub.publish(Vel);
-        ros::spinOnce();                                            //listen to all subscriptions once
+        ros::spinOnce();                                            // Listen to all subscriptions once
         // if (AnyBumperPressed) {
         //     break;
         // }
@@ -215,22 +213,22 @@ void reverse(ros::Publisher VelPub) {                               // Called if
         seconds_elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count(); // Count how much time has passed
     }
     
-    if (array_equal(PressedBumper,l_state)) angle = -45;            // Left bumper, rotate right (right is neg)
+    if (arrayEqual(PressedBumper,LState)) angle = -45;            // Left bumper, rotate right (right is neg)
     
-    else if (array_equal(PressedBumper, l_state)) angle = -90;     // Left and middle pressed only
+    else if (arrayEqual(PressedBumper, LMState)) angle = -90;      // Left and middle pressed only
 
-    else if (array_equal(PressedBumper,r_state)) angle = 45;        // Right pressed only, rotate left (pos)
+    else if (arrayEqual(PressedBumper,RState)) angle = 45;        // Right pressed only, rotate left (pos)
 
-    else if (array_equal(PressedBumper,rm_state)) angle = 90;       // Right and middle pressed only, rotate left
+    else if (arrayEqual(PressedBumper,RMState)) angle = 90;       // Right and middle pressed only, rotate left
 
-    else if (array_equal(PressedBumper,m_state)) {                  // Middle pressed only
+    else if (arrayEqual(PressedBumper,MState)) {                  // Middle pressed only
         // Rotate random angle
         int array_angles[2] = {90, -90};                            // Array of random angles
         int rand_num = rand() % 2;                                  // Random number from 0 to 1
         angle = array_angles[rand_num];                             // Index the array angles using random number, to rotate either left or right 90 degrees
     }
 
-    else if  (array_equal(PressedBumper,lmr_state) || array_equal(PressedBumper,lr_state)) { // All pressed or left and right pressed
+    else if  (arrayEqual(PressedBumper,LMRState) || arrayEqual(PressedBumper,LRState)) { // All pressed or left and right pressed
        // Rotate random angle
         int array_angles[2] = {135, -135};                          // Array of random angles
         int rand_num = rand() % 2;                                  // Random number from 0 to 1
@@ -245,8 +243,8 @@ void rotate(ros::Publisher VelPub, int angle_rot){                  // Called wi
     
    int dir = sign(angle_rot);
 //    ROS_INFO("direction: %i", dir);
-   float angle_rad = DEG2RAD(dir*angle_rot);                         // Function takes in an angle in degrees, converts it to rad
-   float turning_time = angle_rad/TURNING_V;                     // Calculates the time needed to turn based on constant turning speed, and the angle
+   float angle_rad = DEG2RAD(dir*angle_rot);                        // Function takes in an angle in degrees, converts it to rad
+   float turning_time = angle_rad/TURNING_V;                        // Calculates the time needed to turn based on constant turning speed, and the angle
        
    std::chrono::time_point<std::chrono::system_clock> start;        // Initialize timer
    start = std::chrono::system_clock::now();                        // Start new timer at current time
@@ -254,9 +252,9 @@ void rotate(ros::Publisher VelPub, int angle_rot){                  // Called wi
     
    ros::Rate loop_rate(LOOP_RATE);
    Vel.linear.x = 0;
-   Vel.angular.z = dir * TURNING_V;                                       // Set turning speed  
+   Vel.angular.z = dir * TURNING_V;                                 // Set turning speed  
 //    ROS_INFO("direction: %f", dir*TURNING_V);
-    while (ros::ok() && seconds_elapsed <= turning_time) {           // Turn until the turning time needed to complete the angle is passed   
+    while (ros::ok() && seconds_elapsed <= turning_time) {          // Turn until the turning time needed to complete the angle is passed   
         VelPub.publish(Vel);                                        // Start turning
         loop_rate.sleep();
         seconds_elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count(); //count how much time has passed
@@ -264,7 +262,6 @@ void rotate(ros::Publisher VelPub, int angle_rot){                  // Called wi
 }
 
 
-// uint8_t determineScanType(std::vector<float> xbox_distances) { // Detect environment classification based on scan data
 uint8_t determineScanType() { // Detect environment classification based on scan data
     uint8_t state = 0;
     IsNearWall = true;
@@ -275,19 +272,13 @@ uint8_t determineScanType() { // Detect environment classification based on scan
     bool BP = 0;
     bool CP = 0;
 
-    // float CMinus = XboxRanges[0];
-    // float BMinus = XboxRanges[CENTRE_INDEX - B_OFFSET];
-    // float ACentre = XboxRanges[CENTRE_INDEX];
-    // float BPlus = XboxRanges[CENTRE_INDEX + B_OFFSET];
-    // float CPlus = XboxRanges[SCAN_LENGTH-1];
-
     if (CMinus > C_THRESHOLD) CM = 1;
     if (BMinus > B_THRESHOLD) BM = 1;
     if (ACentre > A_THRESHOLD) AC = 1;
     if (BPlus > A_THRESHOLD) BP = 1;
     if (CPlus > C_THRESHOLD) CP = 1;
 
-    ROS_INFO("5 points left to right: %i, %i, %i, %i, %i", CP, BP, AC, BM, CM);
+    // ROS_INFO("5 points left to right: %i, %i, %i, %i, %i", CP, BP, AC, BM, CM);
 
 
     if (CM && BM && AC && BP) {
@@ -311,8 +302,8 @@ uint8_t determineScanType() { // Detect environment classification based on scan
 
     else if ((!AC && !BP) || (!AC && !BM) || (!BM && !BP)) {
         state = 5;
-        if (CP) TurningBias += 1;            // Turn left
-        if (CM) TurningBias = -1;           // Turn right
+        if (CP) TurningBias += 1;       // Turn left
+        if (CM) TurningBias = -1;       // Turn right
         
     }
 
@@ -359,7 +350,29 @@ int newFrontier() {
     //!!!!!!TO-DO!!!!!!
     // Check Occupancy Grid map
     //!!!!!!TO-DO!!!!!!
+
+    // delta (float, default: 0.05)
+    // Resolution of the map (in metres per occupancy grid block)
+
+    // https://answers.ros.org/question/10268/where-am-i-in-the-map/
+    
+    // Map metadata
+    // # This hold basic information about the characterists of the OccupancyGrid
+
+    // # The time at which the map was loaded
+    // time map_load_time
+    // # The map resolution [m/cell]
+    // float32 resolution
+    // # Map width [cells]
+    // uint32 width
+    // # Map height [cells]
+    // uint32 height
+    // # The origin of the map [m, m, rad].  This is the real-world pose of the
+    // # cell (0,0) in the map.
+    // geometry_msgs/Pose origin
+
     return 0;
+
 }
 
 
@@ -367,25 +380,24 @@ int newFrontier() {
 // !!!!!!!!!!!!!MAIN!!!!!!!!!!!!!!!!!!!!!!!!!!
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "image_listener"); //node name
-    ros::NodeHandle nh;                      //initialize node handler
-    ros::Rate loop_rate(LOOP_RATE); //code will try to run at 10 Hz
+    ros::init(argc, argv, "image_listener");                    // Node name
+    ros::NodeHandle nh;                                         // Iinitialize node handler
+    ros::Rate loop_rate(LOOP_RATE);                             // Code will try to run at LOOP_RATE Hz
 
     ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback); //subscribe to bump topic with callback
-    ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback); //subscribe  to /scan topic
-    ros::Subscriber odom = nh.subscribe("odom", 1, &odomCallback); //NEW
-    VelPub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1); //publish cmd_velocity as Twist message
+    ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);       // Subscribe  to /scan topic
+    ros::Subscriber odom = nh.subscribe("odom", 1, &odomCallback);
+    VelPub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1); // Publish cmd_velocity as Twist message
 
-    uint64_t seconds_elapsed = 0;               // Variable for time that has passed
-    uint8_t scan_mode = 0;                      // List of X states
+    uint64_t seconds_elapsed = 0;                               // Variable for time that has passed
+    uint8_t scan_mode = 0;                                      // List of X states
     int checkpoint_counter = 1;             
     float rotation = 0;                             
-    float dist_travelled = 0;                   // How much moved since last checkpoint
-    std::vector<float> last_odom{0,0};
-    // float last_odom[2] = {0,0};                 // Location of last point
-    long turning_count = 0;                     // Accumulation of how much travelled
-    bool just_rotated = false;                  // Did we just make a 90 degree turn?
-    bool is_in_corner = false;                  // Did we just make a 90 degree turn?
+    float dist_travelled = 0;                                   // How much moved since last checkpoint
+    std::vector<float> last_odom{0,0};                          // Location of last point
+    long turning_count = 0;                                     // Accumulation of how much travelled
+    bool just_rotated = false;                                  // Did we just make a 90 degree turn?
+    bool is_in_corner = false;                                  // Did we just make a 90 degree turn?
     int decision = 0;
     int scan_cmd = 0;
     int occup_cmd = 0;
@@ -409,12 +421,12 @@ int main(int argc, char **argv)
             dist_travelled = 0;
             OdomArray[checkpoint_counter] = CurrOdom;   // Keeps track of locations of all past checkpoints, leaves first index as 0,0
             checkpoint_counter += 1;
-            rotate(VelPub, 360);                                // Scan area. positive is counterclockwise
+            rotate(VelPub, 360);                        // Scan area. positive is counterclockwise
             // rotate(VelPub, 600); // Just for simulation
             continue;
         }
         
-        scan_mode = determineScanType();                //recognize different possibilities based on scan array, also look at changes in odom
+        scan_mode = determineScanType();                // Recognize different possibilities based on scan array, also look at changes in odom
         ROS_INFO("Scan Mode: %i", scan_mode);
         if (scan_mode < 3) {
             IsNearWall? forward(VelPub, FORWARD_SLOW_V, STEP_SIZE): forward(VelPub, FORWARD_FAST_V, STEP_SIZE);  
@@ -422,13 +434,13 @@ int main(int argc, char **argv)
 
         else if ((scan_mode == 3) || (scan_mode == 4)){ //Tilt
             rotation = TurningBias*ADJUST_ANGLE;
-            rotate(VelPub, rotation);                           // Fix allignment by ~ 15 degrees depending on direction of bias
+            rotate(VelPub, rotation);                   // Fix allignment by ~ 15 degrees depending on direction of bias
             turning_count += rotation;                  // Keep track of turning
         }
 
         else if (scan_mode == 5) {                      // Front wall
             if (just_rotated) {                         // Bad turning decision, we're at a corner and turned into other wall
-                rotate(VelPub, 180);                            // Turn around, think about a dead end corridor situation
+                rotate(VelPub, 180);                    // Turn around, think about a dead end corridor situation
                 is_in_corner = true;
                 turning_count += -decision*180;         // If we made the wrong choice before, we want to overwrite that contribution to the count and add accordingly
                 just_rotated = false;                   // Reset flag
@@ -443,7 +455,7 @@ int main(int argc, char **argv)
             }
 
             if (turning_count > TURNING_LIM) {          // We've been turning too much
-                rotate(VelPub, 180);                            // Go other direction
+                rotate(VelPub, 180);                    // Go other direction
                 turning_count = 0;                      // Reset flag
                 continue;
             }
@@ -462,13 +474,13 @@ int main(int argc, char **argv)
 
 
 
-            decision = sign(scan_cmd*SCAN_WEIGHT + odom_cmd*ODOM_WEIGHT + occup_cmd*OCCUP_WEIGHT); //weighted decision of costs
+            decision = sign(scan_cmd*SCAN_WEIGHT + odom_cmd*ODOM_WEIGHT + occup_cmd*OCCUP_WEIGHT); // Weighted decision of costs
             ROS_INFO("Scan: %i, Odom: %i,  Occupancy: %i, Decision: %i", scan_cmd, odom_cmd, occup_cmd, decision);
             //TO-DO!!!!!!!!!!
             //rather than weight, potentially do steps to see if that area's been explored
             //TO-DO!!!!!!!!!!
             rotation = 90*decision;
-            rotate(VelPub, rotation);                           // Rotate 90 based on decision
+            rotate(VelPub, rotation);                   // Rotate 90 based on decision
             turning_count += rotation;                  // Add rotation to count
             just_rotated = true;
         }
