5d9bf24b375c78c92803be29a8b311976d4289da
AdamCavallo <123097786+AdamCavallo@users.noreply.github.com>
2023-03-13 16:35:28 -0400

    Update imagePipeline.cpp

--- mie443_contest2/src/imagePipeline.cpp
+++ mie443_contest2/src/imagePipeline.cpp
@@ -46,6 +46,79 @@ int ImagePipeline::getTemplateID(Boxes& boxes) {    //This is called in main con
     } else {     //(if the image is valid)
         /***YOUR CODE HERE***/                 /// this should compare the boxes images (passed in the argument) to the video str. It seems boxes.templates (xml) is a vector of 3 files, and we have 4 cases
         // Use: boxes.templates                 ///i think we need to compare the stream with boxes.templates(1), (2) and (3) and have 'else' be the blank case. Then set template_id =1 for the first 1, or 2 for second... and 4 for the else case
+        
+        ///for loop, for each match, remove item from a master list, once the master list reaches 1, that last one is automatically returned
+        
+        Mat img_object =  boxes.templates[1];  ///boxes images // make sure this is the correct reference 
+        Mat img_scene = img;   //  Sensor images
+        if ( img_object.empty() || img_scene.empty() )
+        {
+            cout << "Could not open or find the image!\n" << endl;
+            parser.printMessage();
+            return -1;
+        }
+        //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+        int minHessian = 400;
+        Ptr<SURF> detector = SURF::create( minHessian );
+        std::vector<KeyPoint> keypoints_object, keypoints_scene;
+        Mat descriptors_object, descriptors_scene;
+        detector->detectAndCompute( img_object, noArray(), keypoints_object, descriptors_object );
+        detector->detectAndCompute( img_scene, noArray(), keypoints_scene, descriptors_scene );
+        //-- Step 2: Matching descriptor vectors with a FLANN based matcher
+        // Since SURF is a floating-point descriptor NORM_L2 is used
+        Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+        std::vector< std::vector<DMatch> > knn_matches;
+        matcher->knnMatch( descriptors_object, descriptors_scene, knn_matches, 2 );
+        //-- Filter matches using the Lowe's ratio test
+        const float ratio_thresh = 0.75f;
+        std::vector<DMatch> good_matches;
+        for (size_t i = 0; i < knn_matches.size(); i++)
+        {
+            if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
+            {
+                good_matches.push_back(knn_matches[i][0]);
+            }
+        }
+        //-- Draw matches
+        Mat img_matches;
+        drawMatches( img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1),
+                    Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+        //-- Localize the object
+        std::vector<Point2f> obj;
+        std::vector<Point2f> scene;
+        for( size_t i = 0; i < good_matches.size(); i++ )
+        {
+            //-- Get the keypoints from the good matches
+            obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+            scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+        }
+        Mat H = findHomography( obj, scene, RANSAC );
+        //-- Get the corners from the image_1 ( the object to be "detected" )
+        std::vector<Point2f> obj_corners(4);
+        obj_corners[0] = Point2f(0, 0);
+        obj_corners[1] = Point2f( (float)img_object.cols, 0 );
+        obj_corners[2] = Point2f( (float)img_object.cols, (float)img_object.rows );
+        obj_corners[3] = Point2f( 0, (float)img_object.rows );
+        std::vector<Point2f> scene_corners(4);
+        perspectiveTransform( obj_corners, scene_corners, H);
+        //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+        line( img_matches, scene_corners[0] + Point2f((float)img_object.cols, 0),
+            scene_corners[1] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4 );
+        line( img_matches, scene_corners[1] + Point2f((float)img_object.cols, 0),
+            scene_corners[2] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+        line( img_matches, scene_corners[2] + Point2f((float)img_object.cols, 0),
+            scene_corners[3] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+        line( img_matches, scene_corners[3] + Point2f((float)img_object.cols, 0),
+            scene_corners[0] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+        //-- Show detected matches
+        imshow("Good Matches & Object detection", img_matches );
+        waitKey();
+
+   
+
+        
+        
+        
         cv::imshow("view", img);   ///img is the video stream, we compare this with boxes, and display the stream image
         cv::waitKey(10);
     }  
@@ -57,83 +130,4 @@ int ImagePipeline::getTemplateID(Boxes& boxes) {    //This is called in main con
                       // we need to modify this to include video stream ... we should look at the video stream topic to see how it gives info
                        // while no match, cycle through template images comparing each with the video stream, if no match (and some other criteria) return template_id = 4 (ex for blank)
 
-const char* keys =
-        "{ help h |                          | Print help message. }"
-        "{ input1 | ../data/box.png          | Path to input image 1. }"
-        "{ input2 | ../data/box_in_scene.png | Path to input image 2. }";
-int main( int argc, char* argv[] ) ///first argument is the image object (the boxes), second argument is the scene image (the sensor)
-{
-    CommandLineParser parser( argc, argv, keys );
-    Mat img_object = imread( parser.get<String>("input1"), IMREAD_GRAYSCALE );   ///boxes images
-    Mat img_scene = imread( parser.get<String>("input2"), IMREAD_GRAYSCALE );   //  Sensor images
-    if ( img_object.empty() || img_scene.empty() )
-    {
-        cout << "Could not open or find the image!\n" << endl;
-        parser.printMessage();
-        return -1;
-    }
-    //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
-    int minHessian = 400;
-    Ptr<SURF> detector = SURF::create( minHessian );
-    std::vector<KeyPoint> keypoints_object, keypoints_scene;
-    Mat descriptors_object, descriptors_scene;
-    detector->detectAndCompute( img_object, noArray(), keypoints_object, descriptors_object );
-    detector->detectAndCompute( img_scene, noArray(), keypoints_scene, descriptors_scene );
-    //-- Step 2: Matching descriptor vectors with a FLANN based matcher
-    // Since SURF is a floating-point descriptor NORM_L2 is used
-    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
-    std::vector< std::vector<DMatch> > knn_matches;
-    matcher->knnMatch( descriptors_object, descriptors_scene, knn_matches, 2 );
-    //-- Filter matches using the Lowe's ratio test
-    const float ratio_thresh = 0.75f;
-    std::vector<DMatch> good_matches;
-    for (size_t i = 0; i < knn_matches.size(); i++)
-    {
-        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
-        {
-            good_matches.push_back(knn_matches[i][0]);
-        }
-    }
-    //-- Draw matches
-    Mat img_matches;
-    drawMatches( img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1),
-                 Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-    //-- Localize the object
-    std::vector<Point2f> obj;
-    std::vector<Point2f> scene;
-    for( size_t i = 0; i < good_matches.size(); i++ )
-    {
-        //-- Get the keypoints from the good matches
-        obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
-        scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
-    }
-    Mat H = findHomography( obj, scene, RANSAC );
-    //-- Get the corners from the image_1 ( the object to be "detected" )
-    std::vector<Point2f> obj_corners(4);
-    obj_corners[0] = Point2f(0, 0);
-    obj_corners[1] = Point2f( (float)img_object.cols, 0 );
-    obj_corners[2] = Point2f( (float)img_object.cols, (float)img_object.rows );
-    obj_corners[3] = Point2f( 0, (float)img_object.rows );
-    std::vector<Point2f> scene_corners(4);
-    perspectiveTransform( obj_corners, scene_corners, H);
-    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
-    line( img_matches, scene_corners[0] + Point2f((float)img_object.cols, 0),
-          scene_corners[1] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4 );
-    line( img_matches, scene_corners[1] + Point2f((float)img_object.cols, 0),
-          scene_corners[2] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-    line( img_matches, scene_corners[2] + Point2f((float)img_object.cols, 0),
-          scene_corners[3] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-    line( img_matches, scene_corners[3] + Point2f((float)img_object.cols, 0),
-          scene_corners[0] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-    //-- Show detected matches
-    imshow("Good Matches & Object detection", img_matches );
-    waitKey();
-    return 0;
-}
-#else
-int main()
-{
-    std::cout << "This tutorial code needs the xfeatures2d contrib module to be run." << std::endl;
-    return 0;
-}
-#endif
+
