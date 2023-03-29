2adc1429a2401e4d4eab103da8b6614ab8b85148
andresc44 <andres.cervera.rozo@gmail.com>
2023-03-29 00:15:26 -0400

    Added contest3 files and folder

---
+++ mie443_contest3/src/contest3.cpp
@@ -0,0 +1,77 @@
+#include <header.h>
+#include <ros/package.h>
+#include <imageTransporter.hpp>
+#include <chrono>
+
+using namespace std;
+
+geometry_msgs::Twist follow_cmd;
+int world_state;
+
+void followerCB(const geometry_msgs::Twist msg){
+    follow_cmd = msg;
+}
+
+void bumperCB(const geometry_msgs::Twist msg){
+    //Fill with code
+}
+
+//-------------------------------------------------------------
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "image_listener");
+	ros::NodeHandle nh;
+	sound_play::SoundClient sc;
+	string path_to_sounds = ros::package::getPath("mie443_contest3") + "/sounds/";
+	teleController eStop;
+
+	//publishers
+	ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop",1);
+
+	//subscribers
+	ros::Subscriber follower = nh.subscribe("follower_velocity_smoother/smooth_cmd_vel", 10, &followerCB);
+	ros::Subscriber bumper = nh.subscribe("mobile_base/events/bumper", 10, &bumperCB);
+
+    // contest count down timer
+	ros::Rate loop_rate(10);
+    std::chrono::time_point<std::chrono::system_clock> start;
+    start = std::chrono::system_clock::now();
+    uint64_t secondsElapsed = 0;
+
+	imageTransporter rgbTransport("camera/image/", sensor_msgs::image_encodings::BGR8); //--for Webcam
+	//imageTransporter rgbTransport("camera/rgb/image_raw", sensor_msgs::image_encodings::BGR8); //--for turtlebot Camera
+	imageTransporter depthTransport("camera/depth_registered/image_raw", sensor_msgs::image_encodings::TYPE_32FC1);
+
+	int world_state = 0;
+
+	double angular = 0.2;
+	double linear = 0.0;
+
+	geometry_msgs::Twist vel;
+	vel.angular.z = angular;
+	vel.linear.x = linear;
+
+	sc.playWave(path_to_sounds + "sound.wav");
+	ros::Duration(0.5).sleep();
+
+	while(ros::ok() && secondsElapsed <= 480){		
+		ros::spinOnce();
+
+		if(world_state == 0){
+			//fill with your code
+			//vel_pub.publish(vel);
+			vel_pub.publish(follow_cmd);
+
+		}else if(world_state == 1){
+			/*
+			...
+			...
+			*/
+		}
+		secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
+		loop_rate.sleep();
+	}
+
+	return 0;
+}
