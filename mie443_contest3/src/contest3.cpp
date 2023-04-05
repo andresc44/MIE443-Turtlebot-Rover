088658a73a139ae22bfc9f4bebfde00095465409
MaggieVuong <m.vuong310@gmail.com>
2023-04-05 18:25:11 -0400

    First iteration of fearMode

--- mie443_contest3/src/contest3.cpp
+++ mie443_contest3/src/contest3.cpp
@@ -7,13 +7,67 @@ using namespace std;
 
 geometry_msgs::Twist follow_cmd;
 int world_state;
-
+// ---------- Callback Functions -------------
 void followerCB(const geometry_msgs::Twist msg){
     follow_cmd = msg;
 }
 
 void bumperCB(const geometry_msgs::Twist msg){
     //Fill with code
+}
+
+// ----------- Fear Function --------------
+void fearMode(ros::Publisher vel_pub, ros::Subscriber ) {
+	int fear_vel = 0.3;
+	int fear_rev_vel = -0.1;
+	int fear_forward_time = 1;
+	int rev_cnt = 0;
+	ros::Rate loop_rate(LOOP_RATE);
+	std::chrono::time_point<std::chrono::system_clock> start;
+	uint64_t seconds_elapsed = 0;                                    
+
+	while (ros::ok() && !AnyBumperPressed && ){	
+		// spin around 
+		rotate(180);
+		rotate(180);
+
+		// go forwards
+		start = std::chrono::system_clock::now();
+		while (ros::ok() && seconds_elapsed<=fear_forward_time){
+			vel_pub.publish(fear_vel);
+			loop_rate.sleep()
+			seconds_elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count(); 
+		}			
+
+		// turn slgihtly and go forwards
+		rotate(30);
+		start = std::chrono::system_clock::now();
+		while (ros::ok() && seconds_elapsed<=0.5){
+			vel_pub.publish(fear_vel);
+			loop_rate.sleep()
+			seconds_elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count(); 
+		}	
+		
+		// turn slightly in other direction and go forwards	
+		rotate(-60);
+		start = std::chrono::system_clock::now();
+		while (ros::ok() && seconds_elapsed<=0.5){
+			vel_pub.publish(fear_vel);
+			loop_rate.sleep()
+			seconds_elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count(); 
+		}	
+
+		// slowly go backwards and express fear sound
+		rotate(30);
+		start = std::chrono::system_clock::now();
+		while (ros::ok() && seconds_elapsed<=0.5 && rev_cnt =3){
+			vel_pub.publish(fear_rev_vel);
+			loop_rate.sleep()
+			seconds_elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count(); 
+			rev_cnt += 1;
+		}	
+
+	}
 }
 
 //-------------------------------------------------------------
