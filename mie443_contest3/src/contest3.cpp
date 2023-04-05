e0f591da09497259040d9bc9a7510b9c124a6eb2
Andres <andres@andres-Ubuntu16.04-Surface>
2023-04-05 00:17:16 -0400

    Fix compile errors

--- mie443_contest3/src/contest3.cpp
+++ mie443_contest3/src/contest3.cpp
@@ -3,6 +3,7 @@
 #include <imageTransporter.hpp>
 #include <chrono>
 #include <kobuki_msgs/BumperEvent.h>
+
 //#include "led_manager/LedAnim.h" // Selin
 
 using namespace std;
@@ -16,18 +17,24 @@ using namespace std;
 #define REVERSE_TIME 8
 #define BACKWARD_T 0.5
 #define FEAR_TIME 10
+#define DEG2RAD(deg) ((deg) * M_PI / 180.)      // Inverse conversion function
 
 sound_play::SoundClient sc;
 geometry_msgs::Twist follow_cmd;
 geometry_msgs::Twist Vel;							// Create message for velocities as Twist type
 ros::Publisher VelPub;
 
+std::string path_to_sounds ("path to strings goes here");
 uint8_t Bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
 bool AnyBumperPressed = false;                  // Reset variable to false
 bool FollowerInReverse = false;
 bool MovingForward = true;
 
 int WorldState;
+
+int sign(float number) {
+    return (number>0)? 1: -1;                               // Positive -> 1, otherwise -1
+}
 
 void followerCB(const geometry_msgs::Twist msg){ 	//Andres
     follow_cmd = msg;
@@ -38,7 +45,7 @@ void followerCB(const geometry_msgs::Twist msg){ 	//Andres
 }
 
 //Adam's branch/////////////////////////////////////
-void bumperCB(const geometry_msgs::Twist msg){
+void bumperCB(const kobuki_msgs::BumperEvent::ConstPtr& msg){
     // Access using bumper[kobuki_msgs::BumperEvent::{}] LEFT, CENTER, or RIGHT
     Bumper[msg->bumper] = msg->state;            	// Assigns bumper array to match message based on msg message
     AnyBumperPressed = false;
@@ -60,7 +67,7 @@ void rotate(ros::Publisher vel_pub, int angle_rot){                  // Called w
    start = std::chrono::system_clock::now();                        // Start new timer at current time
    uint64_t seconds_elapsed = 0;                                    // New variable for time that has passed
     
-   ros::Rate loop_rate(100);
+   ros::Rate loop_rate(LOOP_RATE);
    Vel.linear.x = 0;
    Vel.angular.z = dir * TURNING_V;                                 // Set turning speed  
 //    ROS_INFO("direction: %f", dir*TURNING_V);
@@ -72,8 +79,8 @@ void rotate(ros::Publisher vel_pub, int angle_rot){                  // Called w
 }
 ////////////////////////////////////////////////////////////
 
-int sign(float number) {
-    return (number>0)? 1: -1;                               // Positive -> 1, otherwise -1
+void neutralMode(ros::Publisher vel_pub) {
+	bool placeholder_for_natural_state = true;
 }
 
 void fearMode (ros::Publisher vel_pub) {
@@ -84,16 +91,18 @@ void fearMode (ros::Publisher vel_pub) {
 void sadMode(ros::Publisher vel_pub) {
 	Vel.linear.x = BACKWARD_V;                                      	// Set linear to Twist
 	Vel.angular.z = 0;
-		uint64_t seconds_elapsed = 0;                                   // New variable for time that has passed    
-	Backward_time = 0.5/abs(BACKWARD_V);   //0.5 m backwards
-		start = std::chrono::system_clock::now();                       // Start new timer at current time
+	uint64_t seconds_elapsed = 0;                                   // New variable for time that has passed    
+	float backward_time = 0.5/abs(BACKWARD_V);   //0.5 m backwards
+	ros::Rate loop_rate(LOOP_RATE);
+	std::chrono::time_point<std::chrono::system_clock> start;
+	start = std::chrono::system_clock::now();                       // Start new timer at current time
 
-		while (ros::ok() && seconds_elapsed <= Backward_time) {         // Kobuki diameter is 0.3515m, we want to travel half (3.5s x 0.05m/s) 
-			vel_pub.publish(Vel);                                        // Publish cmd_vel to teleop mux input
-			loop_rate.sleep();
-			seconds_elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count(); // Count how much time has passed
-			//ROS_INFO("seconds_elapsed:%i,backward_T:%f",seconds_elapsed,BACKWARD_T);
-		}
+	while (ros::ok() && seconds_elapsed <= backward_time) {         // Kobuki diameter is 0.3515m, we want to travel half (3.5s x 0.05m/s) 
+		vel_pub.publish(Vel);                                        // Publish cmd_vel to teleop mux input
+		loop_rate.sleep();
+		seconds_elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count(); // Count how much time has passed
+		//ROS_INFO("seconds_elapsed:%i,backward_T:%f",seconds_elapsed,BACKWARD_T);
+	}
 	//////start being sad
 	sc.playWave(path_to_sounds + "sound.wav"); //starts playing crying noises
 	
@@ -105,7 +114,7 @@ void sadMode(ros::Publisher vel_pub) {
 	//delay()
 	rotate(vel_pub, 45);
 	//delay
-	sc.stopPlayingWave(); //stops the sound
+	// sc.stopPlayingWave(); //stops the sound CAUSES CATKIN_MAKE ERROR
 	WorldState = 0;
 }
 
@@ -113,7 +122,7 @@ void excitedMode(ros::Publisher vel_pub) {
 	// Move TurtleBot in circles for 10 seconds
 	Vel.angular.z = 1.0;
 	for (int i=0; i < 10; i++){ 
-		vel_pub.publish(vel);
+		vel_pub.publish(Vel);
 		ros::Duration(1.0).sleep();
 	}
 	
@@ -180,7 +189,7 @@ void rageMode(ros::Publisher vel_pub) {
 	Vel.angular.z = 0.0;
     Vel.linear.x = RAGE_VEL;
     seconds_elapsed = 0;                                    // Variable for time that has passed
-	time_forward = abs(FORWARD_DIST/RAGE_VEL);
+	float time_forward = abs(FORWARD_DIST/RAGE_VEL);
 
     start = std::chrono::system_clock::now();                       // Start the timer
 	ros::spinOnce();
@@ -260,8 +269,25 @@ int main(int argc, char **argv)
 
 	while(ros::ok() && seconds_elapsed <= 480){		
 		ros::spinOnce();
-		if (reverse_start) time_in_reverse = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-reverse_start).count();
-		if (fear_start) time_alone = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-fear_start).count();
+
+// try {
+//         if(isValid) {
+//             img.release(); //previous image is released from memory 
+//         }
+//         img = (cv_bridge::toCvShare(msg, IMAGE_TYPE)->image).clone();
+//         isValid = true;
+//     } catch (cv_bridge::Exception& e) {
+//         std::cout << "ERROR: Could not convert from " << msg->encoding.c_str()
+//                   << " to " << IMAGE_TYPE.c_str() << "!" << std::endl;
+//         isValid = false;
+//     }    
+// }
+
+
+		time_in_reverse = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-reverse_start).count();
+
+		// if (reverse_start) time_in_reverse = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-reverse_start).count();
+		time_alone = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-fear_start).count();
 		
 		if ((follow_cmd.linear.x != 0) || (follow_cmd.angular.z != 0)) following_human = true;
 
@@ -304,17 +330,17 @@ int main(int argc, char **argv)
 				break;
 			case 2:
 				sadMode(VelPub);
-				buffer_start = std::chrono::system_clock::now();
+				// buffer_start = std::chrono::system_clock::now();
 				last_state = 2;
 				break;
 			case 3:
 				excitedMode(VelPub);
-				buffer_start = std::chrono::system_clock::now();
+				// buffer_start = std::chrono::system_clock::now();
 				last_state = 3;
 				break;
 			case 4:
 				rageMode(VelPub);
-				buffer_start = std::chrono::system_clock::now();
+				// buffer_start = std::chrono::system_clock::now();
 				is_reversing = false;
 				last_state = 4;
 				break;
